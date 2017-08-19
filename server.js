e3cca1d8bdaa95bc751241316d2089e71a179390
nitishd24 <nitishkumar24oct@gmail.com>
2017-08-19 23:24:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/:articleName',function(req,res){
-    var articleName = req.parans.articleName;
+    var articleName = req.params.articleName;
   res.send(path.join(createtemplate(articles[articleName])));
 });
 

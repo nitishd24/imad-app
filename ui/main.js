a6ab784fbcf35faccf40a3818f3454e6e3460eb6
nitishd24 <nitishkumar24oct@gmail.com>
2017-08-20 00:08:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,10 @@
 console.log('Loaded!');
+//change content
 var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
+//move element
+var img = document.getElementById('modi');
+img.onclick = function()
+{
+    img.style.marginleft = '100px';
+};

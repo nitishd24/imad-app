ada02376f4fb32784108184610a3113a5a210516
nitishd24 <nitishkumar24oct@gmail.com>
2017-08-19 21:53:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/article-two',function(req,res){
 });
 
 app.get('/article-three',function(req,res){
-   res.send('Article Three is created');    
+    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));  
 });
 
 app.get('/ui/style.css', function (req, res) {

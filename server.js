d5bb00284e40840cba7d1413e647459902db6300
nitishd24 <nitishkumar24oct@gmail.com>
2017-08-25 23:45:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ var htmltemplate = `<!doctype html>
     <body>
         <div class="container">
             <div>
-                <a href='/' class="one" margin-left=0>Home</a>
+                <a href='/' class="a1">Home</a>
             </div>
             <div>
                 <hr/>

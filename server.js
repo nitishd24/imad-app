52880305d6a2b6d72164aa0d002f0df33926da21
nitishd24 <nitishkumar24oct@gmail.com>
2017-08-25 22:54:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -71,9 +71,9 @@ app.get('/:articleName',function(req,res){
   res.send(path.join(createtemplate(articles[articleName])));
 });
 
-var counter=0;
-app.get('/counter',function(req, res) {
-    counter=counter+1;
+var counter = 0;
+app.get('/counter' , function (req, res) {
+    counter = counter + 1;
     res.send(counter.toString());
 });
 app.get('/ui/style.css', function (req, res) {

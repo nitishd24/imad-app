6b394c2257c7e8bec1e0834b309e6257223863f0
nitishd24 <nitishkumar24oct@gmail.com>
2017-08-26 21:16:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,16 @@
-console.log('Loaded!');
-//change content
-var element = document.getElementById('main-text');
-element.innerHTML = 'SEXY Disha!!!!';
-//move element
-var img = document.getElementById('madi');
-var marginLeft =0;
-function moveRight ()
-{
-    marginLeft = marginLeft +1;
-    img.style.marginLeft = marginLeft +'px';
-}
-img.onclick = function()
-{
-    var interval = setInterval(moveRight, 50);
+var button = document.getElementById('counter');
+button.onclick = function()  {
+    var request =new XMLHttpRequest();
+    request.onreadystatechange = function() {
+      if(request.readyState === XMLHttpRequest.DONE) {
+          if(request.status === 200) {
+              var counter = request.responseText;
+              var span = document.getElementById('count');
+              span.innerHTML = counter.toString();
+          }
+          }
+      } 
+    };
+    request.open('GET','http://nitishkumar24oct.imad.hasura-app.io/counter', true);
+    request.send(null);
 };
-
